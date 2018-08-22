4d20fa7a26cdad55eaf9701a0b20b8a5fbb69492
Scott M <gmat191@gmail.com>
2018-08-22 00:33:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var newArr = [];
+	dwarves.forEach(function(name, i) {
+		newArr.push(i+1 +". " + name);
+	});
+	return newStr.join(" ");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    if (planeteerCalls[i] > 4) {
+      return true
+    }
+  }
+  return false
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length ; i++) {
+    if (words[i].length > 4) {
+      return true
+    }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  var cheese = ['swiss', 'cheddar', 'guyere']
+  for (var i = 0; i < foods.length ; i++) {
+    var cheeseIdx = cheese.indexOf(foods[i]);
+    if(cheeseIdx !== -1) {
+      return foods[i]
+    }
+  }
+  return "no cheese!"
 }
