e52d582955096f3891ac44b72f6978641b3156d2
Scott M <gmat191@gmail.com>
2018-08-29 19:20:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function dwarfRollCall(dwarves) {
+
   var string = "";
-  dwarves.forEach(function(name, i) {
-  	string += (i+1 + ". " + name + " ");
+  dwarves.forEach(function(element, index) { //forEach is a callback, takes two parameters.
+  	string += (i+1 + ". " + element + " ");
   });
   return string;
 }
-
+//forEach method only works on arrays, it takes element and index or if renamed, name and i(index).
 function summonCaptainPlanet(planeteerCalls){
   let summon = []; //defined new variable initialized to new array []
   for (var i = 0; i < planeteerCalls.length; i++) { //iterate through our calls
@@ -26,8 +27,8 @@ function longPlaneteerCalls(words) {
 function findTheCheese (foods) {
   var cheese = ['camembert', 'cheddar', 'gouda'] //array containing only 3 cheeses
   for (var i = 0; i < foods.length ; i++) { //iterate through that array
-    var cheeseIndex = cheese.indexOf(foods[i]); //indexOf() method searches for a specific element
-    if(cheeseIndex !== -1) {
+    var cheeseIndex = cheese.indexOf(foods[i]); //indexOf() method searches the entire array for that element.
+    if(cheeseIndex !== -1) { //!== checks value and type
       return foods[i] //return our entire foods array
     }
   }
